37987c58baf70b84888c3bdf9122a40f79672e99
Arthur-Pal <zz9999z@yahoo.com.tw>
2021-09-03 14:54:40 +0800

    20210903 Arthur Commit
    20210903 Arthur Commit

---
+++ js/ShipAlert.js
@@ -0,0 +1,12 @@
+﻿
+//開始函數
+$(document).ready(function () {
+    //一進入畫面讀取CCTV 攝影機URL
+    console.log("Enter Ready");
+});
+
+//儲存 Ship Data
+function SaveShipAlert()
+{
+    console.log("SaveShipAlert");
+}
